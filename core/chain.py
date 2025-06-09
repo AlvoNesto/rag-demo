cf9ae11449810edc15162265c9c96f80ece9b67e
Alvaro Narvaez <Alvaro.Narvaez@partner.ibm.com>
2025-06-09 11:32:53 -0500

    add .env

--- core/chain.py
+++ core/chain.py
@@ -2,17 +2,21 @@ from langchain_openai import ChatOpenAI
 from langchain_community.chat_models import ChatOllama
 from langchain.chains import ConversationChain
 from core.base_memory import PersistentConversationSummaryMemory
+from dotenv import load_dotenv
+import os
 
-# llm = ChatOpenAI(
-#     model="gpt-4",
-#     temperature=0.7,
-#     openai_api_key=""
-# )
+load_dotenv()
 
-llm = ChatOllama(
-    model="mistral",
-    temperature=0.7
+llm = ChatOpenAI(
+    model="gpt-4",
+    temperature=0.7,
+    openai_api_key=os.getenv("OPENAI_API_KEY")
 )
+
+# llm = ChatOllama(
+#     model="mistral",
+#     temperature=0.7
+# )
 
 def get_conversation_chain():
     memory = PersistentConversationSummaryMemory(
